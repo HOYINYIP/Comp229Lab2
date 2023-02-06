c4d891a1e4eca8046ab156802df7e1470ccc42bd
HOYINYIP <jones.yipkgi@gmail.com>
2023-02-05 22:03:53 -0500

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,12 @@ router.get('/home', function(req, res, next) {
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('index', { title: 'About me'});
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects'});
 });
 
 /* GET Services page. */
