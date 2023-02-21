3480c160903e21b52dcd12de3c692655cf43bc84
HOYINYIP <jones.yipkgi@gmail.com>
2023-02-20 23:00:42 -0500

    UIR update

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts"
+   // "URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://jonesyip:Jones98030387@mongodbserver.hnecs14.mongodb.net/business_contacts?retryWrites=true&w=majority"
+        
 }
